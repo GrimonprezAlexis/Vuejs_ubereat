4e1b668d57278e39d385bcc9731551e026a18b80
Alex <alexis.autres@gmail.com>
2023-10-02 19:32:07 +0200

    App Ubereats

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+     <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+@import url('./style/reset.css');
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap');
+#app {
+  font-family: 'Roboto', sans-serif;
+  padding: 0px 20px;
+}
+</style>
---
+++ src/page/Restaurant.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>Restaurant</p>
+  {{ $route.params.name }}
+</template>
+
+<script>
+export default {
+  name: 'Restaurant'
+}
+</script>
+
+<style>
+
+</style>
